719ed8b5b860f961c4127020fbeccfee55b06bf0
ekimaycicek <ekimaycicek@icloud.com>
2023-09-15 15:27:13 +0300

    new

--- src/main/java/Market.java
+++ src/main/java/Market.java
@@ -29,6 +29,7 @@ public class Market {
     static double weeklyTurnover=0;
 
     public static void main(String[] args) {
+
         int day=0;
         while (day<7){
             System.out.println("please "+ days.get(day) + " enter the revenue of the day: ");
